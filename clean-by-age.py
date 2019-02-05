16e5f4882d46b2ca8d48f70137a4e549b6f1c634
Warren Fisher <github_fisher@wfisher.info>
2019-02-05 17:13:19 -0400

    Initial class method commit.

--- clean-by-age.py
+++ clean-by-age.py
@@ -16,7 +16,7 @@ incl_folders and incl_files are for setting whether script will delete folders, 
 """
 dirname = r'c:\Users\warren\Downloads'
 percent_full_delete_threshold = 0.95 # If disk passes this threshold use second value for delete_older_than. 
-delete_older_than = [60*60*24*30, 60*60*24*15] # Time, in seconds, before a file is deleted. The first value applied always, the second for when your disk is atleast as full as specified.
+delete_older_than = [1, 1] # Time, in seconds, before a file is deleted. The first value applied always, the second for when your disk is atleast as full as specified.
 incl_folders = True # Should this delete folders
 incl_files = True # Should this delete files 
 
@@ -24,93 +24,127 @@ incl_files = True # Should this delete files
 incl_file_type = [] # Only delete files of extension type .xyz
 excl_file_type = ['.txt'] # Do not delete files with extension type .xyz, overpowers whitelist
 
-def delete_files(paths, include_folders=False, include_files=True): 
-	""" Delete files and folders within the input list of form [files, folders]. """
-	files, folders = paths
-	for file in files:
-		try: 
-			os.remove(file) 
-		except OSError as e: # If the file does not exist we will get this error. 
-			print("No file error: {} - {}".format(e.filename, e.strerror))
-	for folder in folders: 
+class Directory():
+	"""
+	A directory class is created so that class instances can be created to track user preferences of multiple directories.
+	"""
+	def __init__(self, path, incl_files, incl_folders, **kwargs):
+		self.path = path
+		self.incl_files = incl_files
+		self.incl_folders = incl_folders
+
+		disk_info = shutil.disk_usage(dirname) # Named tuple = usage(total, used, free)
+		self.percent_full = disk_info[2]/disk_info[0]
+
+		st = os.stat(self.path) # Creates a temporary named tuple
+
+		# We run the age on __init__ because we will not save the file objects,
+		# rather they will be initiated each time the script is run. 
+		self.age = time.mktime(time.gmtime())-st.st_mtime # Equivalent to current_time - modified_time, in seconds
+
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+
+	def recursive_directories(self):
+		"""
+		Outside function to be called to initiate deleting of files. First it 
+		First it checks if recursive directory mode is enabled, and if so recalls itself.  
+		Once a directory is found that recursive directory mode is disabled, or the maximum search depth of 5 is reached,
+		the method then calls delete_files() for all directories searched. 
+		Finally if this is not the base directory we delete it, if set.
+		"""
+		self.get_folders()
+		for folder in self.folders:
+			if folder.recursive == True and folder.depth < 5: 
+				folder.recursive_directories()
+			elif folder.recursive == False: 
+				return
+
+		if self.incl_files == True:
+			self.delete_files()
+
 		try:
-			shutil.rmtree(folder)
+			self.depth
+			if self.incl_files == True: 	
+				self.delete()
+		except AttributeError: 
+			# If we get an attribute error we know that this is the base directory, we don't want to delete it! 
+			pass 
+
+	def get_folders(self):
+		"""
+		Method to list all subdirectories within the base directory.
+		Creates a new class instance for each folder. 
+		Not done at class initiation because it will create class instances, and therefore ages, for each folder. 
+		If the age were not done at script runtime then the folder age would be outdated. 
+		"""
+		self.folders = [] 
+		for f in os.listdir(self.path):
+			if not os.path.isfile(os.path.join(self.path, f)):
+				try:
+					self.depth
+					self.folders.append(Directory(os.path.join(self.path, f), self.incl_files, self.incl_folders, depth = self.depth+1, recursive = True))
+				except AttributeError: 
+					self.folders.append(Directory(os.path.join(self.path, f), self.incl_files, self.incl_folders, depth = 1, recursive = True))
+
+	def delete_files(self):
+		"""
+		Method called by recursive_directories() to delete files.
+		First calls get_files() to create the class attribute files, and then deletes all files older than the required age. 
+		"""
+		self.get_files()
+		for file in self.files:
+			if file.age >= delete_older_than[0]:
+				file.delete()
+
+	def get_files(self):
+		"""
+		Method to list all files in the base directory. 
+		We only call this method when we are going to delete files because it creates class instances for each file and therefore their file age. 
+		"""
+		self.files = []
+		for f in os.listdir(self.path):
+			if os.path.isfile(os.path.join(self.path, f)):
+				self.files.append(File(os.path.join(self.path, f)))
+
+	def delete(self):
+		"""
+		Method to delete the directory. 
+		"""
+		try:
+			shutil.rmtree(self.path)
 		except OSError as e: # If the folder does not exist we will get this error. 
 			print("No folder error: {} - {}".format(e.filename, e.strerror))
 
-def extension(paths, incl_file_type=[], excl_file_type=[]): # Work on implementing this into wrapper
-	files, folders = paths
-	files_to_delete = []
-	if incl_file_type == excl_file_type == []:
-		return paths # If there are no blacklist/whitelist options return original files/folder locations
-	for file in files:
-		if incl_file_type == []: 
-			files_to_delete = files
-		else:
-			for file_type in incl_file_type:
-				if file.lower().endswith(file_type) == True:
-					files_to_delete.append(file)
-		for file_type in excl_file_type: 
-			try:
-				if file.lower().endswith(file_type) == True:
-					files_to_delete.remove(file)
-			except ValueError:
-			# If a file is not in the files_to_delete list it will throw this error.
-			# This can happen if a user sets a whitelist as well as a blacklist with different file extension types. 
-				print("Error: You set a whitelist and blacklist at the same time, which is not recommended") 
-	return [files_to_delete, folders]				
+class File():
+	"""
+	A file class to store attributes such as age, path, and extension. 
+	"""
+	def __init__(self, path):
+		"""
+		Path is an os.path instance. 
 
-def older_than_decorate(func): 
-	"""Decorator for the directory_list function. Wraps a file age test. """
-	def wrapper(dirname, older_than = 7*24*60*60, *args, **kwargs):
-		paths = func(dirname, *args, **kwargs) # Call our directory_list function to return a list of all files and folders (non-recursive) in format [filers, folders].
-		files, folders = paths
-		files_older = []
-		folders_older = []
-		for file in files:
-			file = os.path.join(dirname, file) # Gives us the absolute path of a file 
-			try: 
-				st = os.stat(file)
-				if time.mktime(time.gmtime())-st.st_mtime >= older_than: # The left hand side equates to the age, in seconds, of a file
-					files_older.append(file)
-			except FileNotFoundError as e: 
-				print("Error: {} = {}".format(e.filename, e.strerror))
-		for folder in folders:
-			folder = os.path.join(dirname, folder) 
-			try: 
-				st = os.stat(folder)
-				if time.mktime(time.gmtime())-st.st_mtime >= older_than: # The left hand side equates to the age, in seconds, of a file
-					folders_older.append(folder)
-			except FileNotFoundError as e: 
-				print("Error: {} = {}".format(e.filename, e.strerror))
-		return [files_older, folders_older] # Returns folders and files older than the desired age. 
-	return wrapper # Returns the wrapper function so that when we decorate it to directory_list it is called instead of it. 
-	
-@older_than_decorate
-def directory_list(dirname, include_files = True, include_folders = False): 
-	"""List all files and or sub-directories within a directory. Will not recursively list all files within sub-directories.""" 
-	dir = [] 
-	if include_files == True: 
-		onlyfiles = [f for f in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, f))]
-		dir.append(onlyfiles)
-	else:
-		dir.append([]) # Append empty list so that it appears there are no files to delete.
-	if include_folders == True: 
-		onlyfolders = [f for f in os.listdir(dirname) if not os.path.isfile(os.path.join(dirname, f))] 
-		dir.append(onlyfolders)
-	else:
-		dir.append([]) # Append empty list so that it appears there are no folders to delete.
-	return dir		
+		For finding the extension of a file if it starts with a leading period, e.g: '.gitignore', 
+		the period is ignored since it's not a file extension. 
+		"""
+		self.path =  path
+		_, self.extension = os.path.splitext(self.path) # The root of the file is not important
+
+		st = os.stat(self.path) # Creates a temporary named tuple
+
+		# We run the age on __init__ because we will not save the file objects,
+		# rather they will be initiated each time the script is run. 
+		self.age = time.mktime(time.gmtime())-st.st_mtime # Equivalent to current_time - modified_time, in seconds
+
+	def delete(self):
+		"""
+		Method to delete the file.
+		"""
+		try: 
+			os.remove(self.path) 
+		except OSError as e: # If the filepath does not exist we will get this error. 
+			print("No file error: {} - {}".format(e.filename, e.strerror))
 
 if __name__ == '__main__': 
-	disk_info = shutil.disk_usage(dirname) # Named tuple usage(total, used, free)
-	try:
-		int(percent_full_delete_threshold)
-		if disk_info[2]/disk_info[0] >= percent_full_delete_threshold:
-			paths = directory_list(dirname, delete_older_than[1], incl_files, incl_folders)
-		else: 
-			paths = directory_list(dirname, delete_older_than[0], incl_files, incl_folders)
-	except ValueError: # If percent_full_delete_threshold is not an integer than we know the user does not want that feature enabled. Act like it wasnt full enough. 
-		paths = directory_list(dirname, delete_older_than[0], incl_files, incl_folders)
-	paths = extension(paths, incl_file_type, excl_file_type)
-	delete_files(paths, incl_folders, incl_files) 
+	dir = Directory(dirname, incl_files, incl_folders, recursive = True)
+	dir.recursive_directories()
