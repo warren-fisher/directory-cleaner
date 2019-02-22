bbf54ba59cdca35b295ee4a93728e4b705105f60
Warren Fisher <github_fisher@wfisher.info>
2019-02-22 13:20:11 -0400

    Added testing fixture

--- test_main.py
+++ test_main.py
@@ -3,7 +3,18 @@ from directory_clean import Directory, File
 import errors
 import os
 
-def test_dotfiles_extension(tmp_path):
+@pytest.fixture(scope="session")
+def create_temp_file():
+    # Returns a helper function to be used to generate files
+    def _temp_file(tmp_path, file_name='', file_extension='', text_string='CONTENT'):
+        directory = tmp_path / "sub"
+        directory.mkdir()
+        path = directory / "{}.{}".format(file_name,file_extension)
+        path.write_text(text_string) # This step creates the file at that location.
+        return path
+    return _temp_file
+
+def test_dotfiles_extension(tmp_path, create_temp_file):
     """
     Creates a temporary .txt file and tests that the File class treats it as having no extension.
     
@@ -11,11 +22,9 @@ def test_dotfiles_extension(tmp_path):
     Subsequently a file named '.txt' is created.
     Using the File class, if its file extension is empty, as expected, the test passes. 
     """
-    d = tmp_path / "sub"
-    d.mkdir()
-    p = d / ".txt"
-    p.write_text('useless text')
-    assert File(p).extension == ''
+    path = create_temp_file(tmp_path, file_extension='txt')
+    print(path)
+    assert File(path).extension == ''
 
 def test_extension():
     """
@@ -31,6 +40,14 @@ def test_fake_file():
     with pytest.raises(errors.NotAFileError):
         File('./fake_file_name')
 
+def test_file_age():
+    """
+    Test to make sure that only files of the appropriate age are deleted. 
+    
+    [description]
+    """
+    pass
+    
 
 
 
