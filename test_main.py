0a539ef692640690aee107d990b8f6598cc1f76f
Warren Fisher <github_fisher@wfisher.info>
2019-03-09 01:06:17 -0400

    Typo

--- test_main.py
+++ test_main.py
@@ -23,7 +23,6 @@ def test_dotfiles_extension(tmp_path, create_temp_file):
     Using the File class, if its file extension is empty, as expected, the test passes. 
     """
     path = create_temp_file(tmp_path, file_extension='txt')
-    print(path)
     assert File(path).extension == ''
 
 def test_extension():
@@ -44,7 +43,8 @@ def test_file_age():
     """
     Test to make sure that only files of the appropriate age are deleted. 
     """
-    pass
+    path = create_temp_file(tmp_path, file_name='age', file_extension='txt')
+    pass 
 
 
     
